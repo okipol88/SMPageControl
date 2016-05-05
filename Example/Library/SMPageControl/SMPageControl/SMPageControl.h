02dada42211a60deb8b18b84037cdb663fd465cd
Błażej Szajrych <blazej.szajrych@gmail.com>
2016-05-05 21:55:39 +0200

    Added lib

---
+++ Example/Library/SMPageControl/SMPageControl/SMPageControl.h
@@ -0,0 +1,20 @@
+//
+//  SMPageControl.h
+//  SMPageControl
+//
+//  Created by Błażej Szajrych on 05.05.2016.
+//  Copyright © 2016 ITGenerator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SMPageControl.h"
+
+//! Project version number for SMPageControl.
+FOUNDATION_EXPORT double SMPageControlVersionNumber;
+
+//! Project version string for SMPageControl.
+FOUNDATION_EXPORT const unsigned char SMPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMPageControl/PublicHeader.h>
+
+
